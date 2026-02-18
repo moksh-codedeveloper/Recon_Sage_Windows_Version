cc7a20866c04e67bc6801b7283f73e703f8b4723
Malde Moksh <maldemoksh437@gmail.com>
2026-02-18 14:31:51 +0530

    Refactor: Remove AppEngine and related WAF/RateLimit modules; clean up project structure

--- Program.cs
+++ Program.cs
@@ -1,10 +1,1 @@
-﻿using AppEngine;
-
-public class Program
-{
-    static async Task Main(string[] args)
-    {
-        AppEngineModel app = new AppEngineModel(Args:args);
-        await app.Run();
-    }
-}
+﻿
