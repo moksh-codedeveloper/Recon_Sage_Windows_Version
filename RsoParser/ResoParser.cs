edbe6b39ba97876fc308ec8006259933ef6b5a3e
Malde Moksh <maldemoksh437@gmail.com>
2026-02-23 14:50:52 +0530

    Add scanning options to RModel and enhance ResoParser for new configuration fields

--- RsoParser/ResoParser.cs
+++ RsoParser/ResoParser.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.Numerics;
 using ResoModel;
 
@@ -46,6 +47,12 @@ namespace ResoParser
 
             if (!int.TryParse(data["timeout"], out int timeout) || timeout <= 0)
                 throw new Exception("Invalid timeout value.");
+            if(!bool.TryParse(data["tor_scan"], out bool tor_scan))
+                throw new Exception("ERROR in parsing the values of tor_scan in bool please pass true or false for if you want it or not");
+            if(!bool.TryParse(data["normal_scan"], out bool normal_scan))
+                throw new Exception("ERROR in parsing the values of normal_scan in bool please pass true or false for if you want it or not");
+            if(!bool.TryParse(data["tor_scan"], out bool adaptive_switch))
+                throw new Exception("ERROR in parsing the values normal_scan in bool please pass true or false for if you want it or not");
 
             // FILE EXTENSION CHECKS
             if (!data["wordlist_path"].EndsWith(".txt", StringComparison.OrdinalIgnoreCase))
@@ -53,7 +60,6 @@ namespace ResoParser
 
             if (!data["json_file_path"].EndsWith(".json", StringComparison.OrdinalIgnoreCase))
                 throw new Exception("Output file must be .json.");
-
             return data;
         }
 
@@ -61,11 +67,14 @@ namespace ResoParser
         {
             return new RModel
             {
-                Target = data.GetValueOrDefault("target"),
-                Concurrency = int.Parse(data.GetValueOrDefault("concurrency")),
-                Timeout = int.Parse(data.GetValueOrDefault("timeout")),
-                JsonFilePath = data.GetValueOrDefault("json_file_path"),
-                WordlistPath = data.GetValueOrDefault("wordlist_path")
+                Target = data["target"],
+                Concurrency = int.Parse(data["concurrency"]),
+                Timeout = int.Parse(data["timeout"]),
+                JsonFilePath = data["json_file_path"],
+                WordlistPath = data["wordlist_path"],
+                TorScan = bool.Parse(data["tor_scan"]),
+                NormalScan = bool.Parse(data["normal_scan"]),
+                AdaptiveSwitch = bool.Parse(data["adaptive_switch"])
             };
         }
     }
